8c9a38f031f94d8f7ef7795d793aa0ba4791f3df
Alexei <alexeim763@gmail.com>
2017-09-29 11:36:19 -0500

    Add edit form

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -5,4 +5,24 @@ class FilmsController < ApplicationController
   def new
     @film = Film.new
   end
+
+  def create
+    @film = Film.new films_params
+    if @film.save
+      p "SE CREO SATISFACTORIAMENTE"
+      redirect_to films_path
+    else
+      render :new
+      p "NO SE CREO"
+    end
+  end
+
+  def edit
+    @film = Film.find(params[:id])
+  end
+
+  private
+  def films_params
+    params.require(:film).permit(:title,:year,:rating,:description)
+  end
 end
